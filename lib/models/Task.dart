a4f4679b013b2bbe41b5ae3074f60bda67cf0706
Matteo <passematteo@gmail.com>
2023-08-21 17:50:21 +0200

    Add Task model class

---
+++ lib/models/Task.dart
@@ -0,0 +1,13 @@
+class Task {
+  final String id;
+  final String name;
+  final String description;
+  final int status;
+
+  Task({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.status
+  });
+}

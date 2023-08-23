99fb7dd6d207af2b1b701ab932120480338297b7
Matteo <passematteo@gmail.com>
2023-08-23 21:01:13 +0200

    Add delete alert dialog
    - Adding a custom delete alert dialog

---
+++ lib/widgets/delete_alert_dialog.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:project_manager/models/Project.dart';
+import 'package:project_manager/models/Task.dart';
+
+class DeleteAlertDialog extends AlertDialog {
+
+  final dynamic object;
+
+  late String name;
+  late String type;
+  late void delete;
+
+  DeleteAlertDialog({super.key, required this.object}){
+
+    switch (object.runtimeType){
+      case Project:
+        Project project = object as Project;
+        type = "project";
+        name = project.name;
+        break;
+      case Task:
+        Task task = object as Task;
+        type = "task";
+        name = task.name;
+        break;
+      default:
+        name = "Unknown object";
+        type = "Unknown object";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Remove the $type"),
+      content: Text("Remove this $type : $name ?"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("No"),
+        ),
+        TextButton(
+          onPressed: () {
+            delete;
+            Navigator.pop(context);
+          },
+          child: Text("Yes"),
+        ),
+      ],
+    );
+  }
+}

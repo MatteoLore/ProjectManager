9761912bf3be9f0d6686bba0a5fef7bf9fbdc21c
Matteo <passematteo@gmail.com>
2023-08-24 14:27:11 +0200

    Added add/edit task dialog (only layout)
    Added add/edit task dialog with:
    - Task name field
    - Task description field
    - Task status dropdown
    - Back button
    - Submit button

---
+++ lib/widgets/addedit_task_dialog.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:project_manager/models/Task.dart';
+
+class AddEditTaskDialog extends AlertDialog {
+
+  final Task task;
+
+  List<Row> status = [Row(mainAxisAlignment: MainAxisAlignment.center,children: [ Container(width: 10,height: 10,decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.orangeAccent,),), Padding(padding: EdgeInsets.only(left: 5), child: Text("En cours", style: TextStyle(fontStyle: FontStyle.normal, fontSize: 16),))]),
+    Row(mainAxisAlignment: MainAxisAlignment.center,children: [ Container(width: 10,height: 10,decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.red,),), Padding(padding: EdgeInsets.only(left: 5), child: Text("En Attente", style: TextStyle(fontStyle: FontStyle.normal, fontSize: 16),))]),
+    Row(mainAxisAlignment: MainAxisAlignment.center,children: [ Container(width: 10,height: 10,decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.green,),), Padding(padding: EdgeInsets.only(left: 5), child: Text("Terminé", style: TextStyle(fontStyle: FontStyle.normal, fontSize: 16),))]),];
+
+  AddEditTaskDialog({super.key, required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    Row _statusValue = status.first;
+    TextEditingController _nameController = TextEditingController();
+    TextEditingController _descriptionController = TextEditingController();
+    return AlertDialog(
+      title: Text("Create / Edit a task"),
+      content: StatefulBuilder(builder: (BuildContext context, void Function(void Function()) setState) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Name"),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: "Description"),
+            ),
+            Padding(padding: EdgeInsets.all(10)),
+            DropdownButton<Row>(
+              value: _statusValue,
+              elevation: 16,
+              style: const TextStyle(color: Colors.black),
+              underline: Container(
+                color: Colors.transparent,
+              ),
+              onChanged: (Row? value) {
+                setState(() {
+                  _statusValue = value!;
+                });
+              },
+              items: status.map<DropdownMenuItem<Row>>((Row value) {
+                return DropdownMenuItem<Row>(
+                  value: value,
+                  child: value,
+                );
+              }).toList(),
+            )
+          ],
+        );
+      },
+
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.check),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

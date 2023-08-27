c5159b0857b4767c54b094687525e487705c324d
Matteo <passematteo@gmail.com>
2023-08-27 23:37:32 +0200

    Update database class
    Update database class in order to adapt the code to the firedart dependency (which replaces firestore cloud)

--- lib/database/Database.dart
+++ lib/database/Database.dart
@@ -1,40 +1,36 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firedart/firedart.dart';
 
 import '../models/Project.dart';
 import '../models/User.dart';
 
 class Database {
-  final CollectionReference projectsCollection = FirebaseFirestore.instance.collection("projects");
-  final CollectionReference usersCollection = FirebaseFirestore.instance.collection("users");
+  final CollectionReference projectsCollection = Firestore.instance.collection("projects");
+  final CollectionReference usersCollection = Firestore.instance.collection("users");
 
-  Stream<List<Project>> getProjects() {
-    return projectsCollection
-        .orderBy("createdAt", descending: true)
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs.map((doc) {
+  Future<List<Project>> getProjects() async {
+    List<Document> projects = await projectsCollection.orderBy("createdAt", descending: true).get();
+      return projects.map((doc) {
+        var data = doc.map;
         return Project(
-          id: doc["id"],
-          authorId: doc["authorId"],
-          name: doc["authorId"],
-          description: doc["description"],
-          tasks: doc["tasks"],
-          status: doc["status"],
-          dateTime: doc["dateTime"],
-          contributors: doc["contributors"],
-          bannerUrl: doc["bannerUrl"]
+          id: doc.id,
+          authorId: data["authorId"],
+          name: data["authorId"],
+          description: data["description"],
+          tasks: data["tasks"],
+          status: data["status"],
+          dateTime: data["dateTime"],
+          contributors: data["contributors"],
+          bannerUrl: data["bannerUrl"]
         );
       }).toList();
-    });
   }
 
   Future<Project?> getProjectById(String projectId) async {
     try {
-      DocumentSnapshot projectSnapshot =
-      await projectsCollection.doc(projectId).get();
+      DocumentReference projectSnapshot = projectsCollection.document(projectId);
 
-      if (projectSnapshot.exists) {
-        Map<String, dynamic> data = projectSnapshot.data() as Map<String, dynamic>;
+      if (await projectSnapshot.exists) {
+        Map<String, dynamic> data = await projectSnapshot.get() as Map<String, dynamic>;
         return Project(
           id: projectSnapshot.id,
           authorId: data["authorId"],
@@ -73,35 +69,32 @@ class Database {
   }
 
 
-  Stream<List<User>> getUsers() {
-    return usersCollection
-        .orderBy("createdAt", descending: true)
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs.map((doc) {
-        return User(
-          id: doc["id"],
-          username: doc["username"],
-          avatarUrl: doc["avatarUrl"],
-          projectsIds: doc["projectsIds"],
-          ownerProjectsIds: doc["ownerProjectsIds"],
-        );
-      }).toList();
-    });
+  Future<List<User>> getUsers() async {
+    List<Document> users = await usersCollection.orderBy("createdAt", descending: true).get();
+    return users.map((doc) {
+      var data = doc.map;
+      return User(
+        id: doc.id,
+        username: data["username"],
+        avatarUrl: data["avatarUrl"],
+        projectsIds: data["projectsIds"],
+        ownerProjectsIds: data["ownerProjectsIds"],
+      );
+    }).toList();
   }
 
-  Future<User?> getUserById(String userId) async {
+  Future<User?> getUserByEmail(String email) async {
     try {
-      DocumentSnapshot userSnapshot =
-      await usersCollection.doc(userId).get();
+      DocumentReference userSnapshot = usersCollection.document(email);
 
-      if (userSnapshot.exists) {
+      if (await userSnapshot.exists) {
+        Map<String, dynamic> data = await userSnapshot.get() as Map<String, dynamic>;
         return User(
           id: userSnapshot.id,
-          username: userSnapshot["username"],
-          avatarUrl: userSnapshot["avatarUrl"],
-          projectsIds: userSnapshot["projectsIds"],
-          ownerProjectsIds: userSnapshot["ownerProjectsIds"],
+          username: data["username"],
+          avatarUrl: data["avatarUrl"],
+          projectsIds: data["projectsIds"],
+          ownerProjectsIds: data["ownerProjectsIds"],
         );
       } else {
         return null;
@@ -112,13 +105,14 @@ class Database {
     }
   }
 
-  Future<void> addUser(User user) async {
+  Future<void> addUser(User user, String email) async {
     try {
       await usersCollection.add({
         "username": user.username,
         "avatarUrl": user.avatarUrl,
         "projectsIds": user.projectsIds,
         "ownerProjectsIds": user.ownerProjectsIds,
+        "email": email
       });
     } catch (e) {
       print(e);
